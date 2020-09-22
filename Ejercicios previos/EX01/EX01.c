667689e2038bb96ac64ca8a445479ccccdba5b6c
Manuel Rubio <macbookpro@Manuel-Rubio.local>
2020-09-22 18:48:04 +0200

    wat

--- Ejercicios previos/EX01/EX01.c
+++ Ejercicios previos/EX01/EX01.c
@@ -2,8 +2,40 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-char	*ft_strdup(const char *src)
+void			*ft_memcpy(void *dest, const void *src, unsigned int n)
+{
+	char		*s;
+	char		*d;
+
+	if (!dest && !src)
+		return (NULL);
+	s = (char *)src;
+	d = dest;
+	while (n--)
+	{
+		*d = *s;
+		d++;
+		s++;
+	}
+	return (dest);
+}
+
+unsigned int	ft_strlen(const char *str)
+{
+	int x;
+
+	x = 0;
+	while (*str != '\0')
+	{
+		x++;
+		str++;
+	}
+	return (x);
+}
+
+char			*ft_strdup(const char *src)
 {
 	char	*s;
 	int		x;
@@ -25,7 +57,7 @@ char	*ft_strdup(const char *src)
 	- write
 	- printf (para pruebas)
 
-/* Definir dos tipos de variable nuevos (pista: typedef)
+Definir dos tipos de variable nuevos (pista: typedef)
 	{ } t_box: Struct que contenga:
 		-un entero: nb
 		-un caracter: c
@@ -38,7 +70,7 @@ char	*ft_strdup(const char *src)
 		-una variable del tipo 't_box': box
 		-una variable del tipo 't_big_box': big_box
 
-/*Funciones a desarrollar
+Funciones a desarrollar
 { } 2 funciones que inicialicen una variable del tipo 't_box' (una función ha de ser tipo void y otra tipo 't_box')
 { } 2 funciones que inicialicen una variable del tipo 't_big_box' (una función ha de ser tipo void y otra tipo 't_box')
 { } Función que limpie la posible memoria reservada de una 't_big_box'
@@ -69,12 +101,10 @@ typedef struct t_mega_big_box
 
 
 //Ej.1
-void	ej_1_a(void)
+void	ej_1_a(t_box *a)
 {
-	t_box	a;
-
-	a.nb = 7;
-	a.c = 'p';
+	a->nb = 7;
+	a->c = 'p';
 }
 
 t_box	ej_1_b(void)
@@ -83,18 +113,17 @@ t_box	ej_1_b(void)
 
 	b.nb = 42;
 	b.c = 'M';
+	return (b);
 }
 
 //Ej.2
-void	ej_2_a(void)
+void	ej_2_a(t_big_box *d)
 {
-	t_big_box d;
-
-	d.str = ft_strdup("Hola");
-	d.nb = 8;
-	d.c = 'D';
-	d.alias_box_a.c = 'E';
-	d.alias_box_a.nb = 9;
+	d->str = ft_strdup("Hola");
+	d->nb = 8;
+	d->c = 'D';
+	d->alias_box_a.c = 'E';
+	d->alias_box_a.nb = 9;
 }
 
 t_box	ej_2_b(void)
@@ -106,43 +135,41 @@ t_box	ej_2_b(void)
 	e.c = 'D';
 	e.alias_box_a.c = 'E';
 	e.alias_box_a.nb = 9;
+	return ();
 }
 
 //Ej.3
-void	free_tbox(t_big_box *f)
+void	free_tbox(t_big_box *t)
 {
-	free(f->nb);
-	free(f->c);
-	free(f->str);
-	free(f->alias_box_a.nb);
-	free(f->alias_box_a.c);
+	free(t->nb);
+	free(t->c);
+	free(t->str);
+	free(t->alias_box_a.nb);
+	free(t->alias_box_a.c);
 }
 
 //Ej.4
-void	ej_4_a(void)
+void	ej_4_a(t_mega_big_box *f)
 {
-	t_mega_big_box	f;
-
-	f.alias_box_b.nb = 2;
-	f.alias_box_b.c = 'R';
-	f.alias_big_box.c = 'A';
-	f.alias_big_box.nb = 1;
-	f.alias_big_box.str = ft_strdup("Hola hola");
-	f.alias_big_box.alias_box_a.c = 'C';
-	f.alias_big_box.alias_box_a.nb = 9;
+	f->alias_box_b.nb = 2;
+	f->alias_box_b.c = 'R';
+	f->alias_big_box.c = 'A';
+	f->alias_big_box.nb = 1;
+	f->alias_big_box.str = ft_strdup("Hola hola");
+	f->alias_big_box.alias_box_a.c = 'C';
+	f->alias_big_box.alias_box_a.nb = 9;
 }
 
-t_box	ej_4_b(void)
+t_box	ej_4_b(t_mega_big_box *g)
 {
-	t_mega_big_box	g;
-
-	f.alias_box_b.nb = 5;
-	f.alias_box_b.c = 'U';
-	f.alias_big_box.c = 'D';
-	f.alias_big_box.nb = 6;
-	f.alias_big_box.str = ft_strdup("Que tal");
-	f.alias_big_box.alias_box_a.c = 'V';
-	f.alias_big_box.alias_box_a.nb = 0;
+	g->alias_box_b.nb = 5;
+	g->alias_box_b.c = 'U';
+	g->alias_big_box.c = 'D';
+	g->alias_big_box.nb = 6;
+	g->alias_big_box.str = ft_strdup("Que tal");
+	g->alias_big_box.alias_box_a.c = 'V';
+	g->alias_big_box.alias_box_a.nb = 0;
+	return ();
 }
 
 //Ej.5
@@ -166,4 +193,13 @@ void	print_lower_char(t_mega_big_box *r)
 		return (NULL);
 	else
 		return (r->alias_big_box.alias_box_a.c);
+}
+
+
+int		main()
+{
+	t_mega_big_box *z;
+
+	ej_4_a(z);
+	print_lower_char(z);
 }
