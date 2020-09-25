8984aff4d24d27007ace79d02c32452d33df63d3
Manuel Rubio <macbookpro@Manuel-Rubio.local>
2020-09-25 09:41:49 +0200

    v1.1

--- printf/src/ft_check_arg.c
+++ printf/src/ft_check_arg.c
@@ -6,7 +6,7 @@
 /*   By: mrubio <mrubio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 03:02:30 by mrubio            #+#    #+#             */
-/*   Updated: 2020/09/25 03:59:19 by mrubio           ###   ########.fr       */
+/*   Updated: 2020/09/25 09:41:11 by mrubio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int		ft_check_arg(char *s, va_list args)
 	x = 0;
 	if (s[x] == 's')
 		ft_putstr_fd(va_arg(args, char*), 1);
-	else if (s[x] == 'd')
-		ft_putnbr_fd(va_arg(args, double), 1);
+	else if (s[x] == 'd' || s[x] == 'i')
+		ft_putnbr_fd(va_arg(args, int), 1);
 	return (x + 2);
 }
 // Arreglar la dovolucion de la X, para que pase la "%d, %s..."
